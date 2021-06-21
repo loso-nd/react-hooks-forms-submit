8b3f7c10e190ccb28b1cac7d688a01e53313107b
Nahum <losond8@gmail.com>
2021-06-21 10:57:59 -0700

    Implemented form validations to my form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,7 @@ function Form({sendFormDataSomewhere}) {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
   const [submittedData, setSubmittedData] = useState([])
+  const [errors, setErrors] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -18,16 +19,28 @@ function Form({sendFormDataSomewhere}) {
     console.log("I have been Submitted.")
 
     //create an object inside our handle func for cntrl form
-    const formDataCntrl = { firstName: firstName, lastName: lastName }
-    setSubmittedData([...submittedData, formDataCntrl])
-    setFirstName("");
-    setLastName("");
-  }
+    // const formDataCntrl = { firstName: firstName, lastName: lastName }
+    // setSubmittedData([...submittedData, formDataCntrl])
+    // setFirstName("");
+    // setLastName("");
 
+    //form validations ( First and Last)
+    if(firstName.length > 0 ){
+      const formDataCntrl = { firstName: firstName, lastName: lastName }
+      setSubmittedData([...submittedData, formDataCntrl])
+    //By setting state to an empty string, we're keeping our React component state in sync with what the user sees on the page.
+      setFirstName("");
+      setLastName("");
+      setErrors([])
+    }else {
+      setErrors(["First Name is required to be at least 4 characters long"])
+    }
+  }
+console.log(submittedData)
   const listofSubmissions = submittedData.map((user, index) => {
     return (
       <div key={index}>
-        {user.lastName}, {user.firstName}
+        <li>{user.lastName}, {user.firstName}</li>
       </div>
     )}
   );
@@ -39,9 +52,16 @@ function Form({sendFormDataSomewhere}) {
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+      {errors.length > 0 ? errors.map((error, index) => (
+        <p key={index} style={{color: "red"}}>
+          {error}
+        </p> 
+      ))
+        : null  
+      }
     <h2>List of Submissions</h2>
       <ul>
-        <lli>{listofSubmissions}</lli>
+        {listofSubmissions}
       </ul>
     </>
   );
