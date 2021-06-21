fc15cce6862d59d674c1baefa4ef584479eef1fe
Nahum <losond8@gmail.com>
2021-06-21 10:33:42 -0700

    Working with React Forms Submit. Triggering synthetic events, based on user input, updating state by stroing those inputs, then displaying those on the DOM.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form({sendFormDataSomewhere}) {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,37 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log("I have been Submitted.")
+
+    //create an object inside our handle func for cntrl form
+    const formDataCntrl = { firstName: firstName, lastName: lastName }
+    setSubmittedData([...submittedData, formDataCntrl])
+    setFirstName("");
+    setLastName("");
+  }
+
+  const listofSubmissions = submittedData.map((user, index) => {
+    return (
+      <div key={index}>
+        {user.lastName}, {user.firstName}
+      </div>
+    )}
+  );
+
   return (
-    <form>
+    <>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    <h2>List of Submissions</h2>
+      <ul>
+        <lli>{listofSubmissions}</lli>
+      </ul>
+    </>
   );
 }
 
